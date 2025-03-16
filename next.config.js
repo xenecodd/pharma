4037b7c4e8af40a6831ff2e5a410bcb5f31179ee
xenecodd <eyuphandurmus41@gmail.com>
2025-03-16 13:40:22 -0400

    deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: 'export',
   distDir: process.env.NODE_ENV === "production" ? "build" : ".next",
   images: {
     unoptimized: true,
